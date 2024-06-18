3d609269e490189bd8b3ccdb85707ddadcb3be4d
WilliamGrossKC <wgross769@gmail.com>
2024-06-17 18:09:02 -0700

    Initial Commit

---
+++ WYA/WYAApp.swift
@@ -0,0 +1,20 @@
+//
+//  WYAApp.swift
+//  WYA
+//
+//  Created by William Gross on 6/17/24.
+//
+
+import SwiftUI
+
+@main
+struct WYAApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
